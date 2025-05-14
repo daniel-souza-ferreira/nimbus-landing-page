10b2c9eb02b9d7a96aa1bd223a9c4af9a9217f7b
daniel-souza-ferreira <dsferreira300@yahoo.com.br>
2025-05-14 16:39:35 -0300

    Adição de botão para voltar ao inicio e alterações no css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Element } from 'react-scroll';
+import { FaAngleDoubleUp  } from "react-icons/fa";
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -8,25 +10,51 @@ import Features from './components/Features';
 import Testimonials from './components/Testimonials';
 import PricingPlans from './components/PricingPlans';
 import Faq from './components/Faq';
+import NavbarButton from './components/NavbarButton';
 
 import { emitAlert } from './utils/emitAlert';
 
 import './App.css'
 
 function App() {
+  const [initialButtonVisibility, setInitialButtonVisibility] = useState(false);
+  
+  const toogleInitialButtonVisibility = () => {
+    setInitialButtonVisibility(window.scrollY > window.innerHeight);
+  }
+
   useEffect(() => {
     emitAlert();
+
+    window.addEventListener("scroll", toogleInitialButtonVisibility)
+
+    return () => {
+      window.removeEventListener("scroll", toogleInitialButtonVisibility);
+    }
   }, []);
   
   return (
     <div className="app-container">
-      <Navbar />
-      <HeroSection />
+      <Element name="initial-page">
+        <Navbar />
+        <HeroSection />
+      </Element>
       <Features />
       <Testimonials />
       <PricingPlans />
       <Faq />
       <Footer />
+      {
+        initialButtonVisibility && (
+        <div className="initial-button">
+          <NavbarButton
+            className="initial-button"
+            linkTo={"initial-page"} 
+            label={<FaAngleDoubleUp  />}
+          />
+        </div>
+        )
+      }
     </div>
   )
 }
